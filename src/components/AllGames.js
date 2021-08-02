7fd243449ad764651a27c703eed12a24a838a479
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-02 18:47:38 +0530

    Game layout and search working

--- src/components/AllGames.js
+++ src/components/AllGames.js
@@ -1,18 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Game from "./Game";
 import UserContext from "../context/UserContext";
 
 export default function AllGames(props) {
+  const [games, setGames] = useState({});
   const userGames = useContext(UserContext);
+
+  let gamesHavingAchievements = userGames.filter(
+    (game) => game.schema_achievements.length > 0
+  );
+
+  useEffect(() => {
+    setGames((oldGames) => gamesHavingAchievements);
+  }, []);
+
+  const searchTextChangedHandler = (e) => {
+    const filteredGames = gamesHavingAchievements.filter((game) => {
+      return game.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setGames((oldGames) => filteredGames);
+  };
 
   return (
     <div className='allgames'>
-      <h1 className='title'>ALL GAMES</h1>
+      <div className='searchContainer'>
+        <input
+          type='text'
+          className='searchText'
+          placeholder='Search for a game'
+          onChange={searchTextChangedHandler}
+        />
+      </div>
 
       <div className='gamesContainer'>
-        {userGames.map((game) => {
-          return <Game key={game.gameid} game={game} />;
-        })}
+        <div className='gameInnerContainer'>
+          {games.length > 0 &&
+            games.map((game) => {
+              return <Game key={game.game_id} game={game} />;
+            })}
+        </div>
       </div>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,13 +3,31 @@ import React from "react";
 export default function Game(props) {
   const game = props.game;
 
+  const schemaAchievements = game.schema_achievements;
+  const playerAchievements = game.player_achievements;
+  const globalAchievements = game.global_achievements;
+
+  const totalAchievements = schemaAchievements.length;
+  const completedAchievements = playerAchievements.reduce(
+    (acc, achievement) => {
+      if (achievement.achieved) {
+        return acc + 1;
+      }
+      return acc + 0;
+    },
+    0
+  );
+  const completionPercentage = Math.ceil(
+    (completedAchievements / totalAchievements) * 100
+  );
+
   return (
     <div className='gameContainer'>
       <div
         className='game'
         style={{ backgroundImage: `url("${game.header_image}")` }}
       ></div>
-      <h5 className='name'>{game.name || "YET TO IMPLEMENT"}</h5>
+      <h5 className='name'>{game.name || "NO GAME NAME"}</h5>
     </div>
   );
 }
