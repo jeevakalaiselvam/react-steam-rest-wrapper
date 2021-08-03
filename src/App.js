1032e1b3e198dbd8bee93d7dbf2ab17106c33fc1
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-03 23:56:57 +0530

    completionist clone start

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Application</h1>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AllGames from "./pages/AllGames/AllGames";
+import RecentAchievements from "./pages/RecentAchievements/RecentAchievements";
 import "./index.scss";
-import App from "./App";
+import Header from "./components/header/Header";
 
 console.clear();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path='/'>
+        <AllGames />
+      </Route>
+      <Route path='/recentachivements'>
+        <RecentAchievements />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
