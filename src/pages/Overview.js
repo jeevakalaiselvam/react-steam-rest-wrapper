56107fdd7c971150e26a99b843ddec755413f62b
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-04 18:57:55 +0530

    State and UI Design start

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -12,7 +12,7 @@ export default function Homepage() {
   return (
     <>
       <Page>
-        <ContentContainer>Homepage</ContentContainer>
+        <ContentContainer>Overview</ContentContainer>
       </Page>
     </>
   );
