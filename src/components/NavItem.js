17159f40a09f4ff06223a805dc226620f367688d
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-04 16:59:46 +0530

    text hover effect added

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -5,13 +5,24 @@ const ItemContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 1rem;
+  padding: 0.5rem 1rem;
   padding-left: 1rem;
+  color: rgb(198, 205, 211);
+  cursor: pointer;
+
+  &:hover {
+    color: rgb(255, 255, 255);
+    background-color: rgba(122, 132, 148, 0.26);
+  }
 `;
 
-const Icon = styled.div``;
+const Icon = styled.div`
+  cursor: pointer;
+`;
 const Title = styled.div`
   padding-left: 1rem;
+
+  cursor: pointer;
 `;
 
 export default function NavItem(props) {
