cba94af6e260f1a6609cd9de6796c8aed8378b44
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-04 22:45:57 +0530

    Context working

--- src/context/GameProvider.js
+++ src/context/GameProvider.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 import { getAllGamesFromAPI } from "../actions/apiActions";
 import { GamesContext } from "./GameContext";
 
-export default function AuthProvider({ children }) {
+export default function GamesProvider({ children }) {
   const [games, setGames] = useState({});
 
   //Load all games and add it into state
   useEffect(() => {
     const fetchGamesfromAPI = async () => {
-      console.log("EFFECT IN APP COMPONENT GETTING GAMES");
+      console.log("EFFECT IN GAME PROVIDER GETTING GAMES");
       console.log(process.env.REACT_APP_API_ALL_GAMES);
       let newGames = [];
       newGames = await getAllGamesFromAPI();
