3206988594b9c8c00c0fbbdb9975d0db40e6cbe9
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-07 23:51:53 +0530

    v2 refactor

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import GamesProvider from "./context/GameProvider";
-
 console.clear();
 
 ReactDOM.render(<App />, document.getElementById("root"));
