f7c5a0da6de17faac42386948db99ad0c3499023
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-08-08 16:56:33 +0530

    responsive view design fixed for games page

--- src/components/card/GameCardSmall.js
+++ src/components/card/GameCardSmall.js
@@ -1,6 +1,6 @@
 import { Icon } from "@material-ui/core";
 import React, { useState } from "react";
-import { FaTrophy } from "react-icons/fa";
+import { FaMedal, FaTrophy } from "react-icons/fa";
 import styled from "styled-components";
 
 const Card = styled.div`
@@ -15,7 +15,6 @@ const Card = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   cursor: pointer;
-  border: 1px solid ${(props) => (props.hover ? "#f5f5f5" : "#00000000")};
 
   @media only screen and (min-width: 1361px) {
     width: 18%;
@@ -34,10 +33,14 @@ const Card = styled.div`
     height: 10vh;
   }
   @media only screen and (max-width: 960px) and (min-width: 769px) {
-    width: 46%;
+    width: 30%;
     height: 10vh;
   }
-  @media only screen and (max-width: 768px) and (min-width: 481px) {
+  @media only screen and (max-width: 768px) and (min-width: 631px) {
+    width: 30%;
+    height: 10vh;
+  }
+  @media only screen and (max-width: 630px) and (min-width: 481px) {
     width: 46%;
     height: 14vh;
   }
@@ -50,13 +53,16 @@ const Card = styled.div`
 const IconInner = styled.div`
   font-size: 1rem;
   color: #fefefe;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const InnerContainer = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
-  background-color: rgba(10, 17, 25, 0.6);
+  background-color: rgba(10, 17, 25, 0.8);
   display: flex;
   width: 100%;
   flex-direction: row;
@@ -64,23 +70,66 @@ const InnerContainer = styled.div`
 
 const AchievementData = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  color: #fefefe;
+  justify-content: flex-start;
+  padding: 0.25rem 0.5rem;
 `;
 const ToGet = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Medal = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  color: gold;
+  justify-content: flex-end;
+  margin-right: 0.5rem;
+`;
+
+const AchievementCount = styled.div`
+  margin-left: 0.25rem;
 `;
 
 export default function GameCardSmall(props) {
-  const game = props.game;
+  const {
+    image,
+    completion_percentage,
+    completed_achievements_count,
+    total_achievements_count,
+  } = props.game;
+
+  const getRemainingForTarget = () => {
+    return Math.floor(
+      (80 / 100) * total_achievements_count - completed_achievements_count
+    );
+  };
 
   return (
-    <Card image={game.image}>
+    <Card image={image}>
       <InnerContainer>
         <AchievementData>
           <IconInner>
             <FaTrophy />
           </IconInner>
+          <AchievementCount>
+            {completed_achievements_count} / {total_achievements_count}
+          </AchievementCount>
         </AchievementData>
-        <ToGet></ToGet>
+        {completion_percentage < 80 && (
+          <ToGet>{getRemainingForTarget()} more...</ToGet>
+        )}
+        {completion_percentage >= 80 && (
+          <Medal>
+            <FaMedal />
+          </Medal>
+        )}
       </InnerContainer>
     </Card>
   );
